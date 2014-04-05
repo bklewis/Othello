ac4a39a108374e0ecd2706c15d7caae74d62469d
Jia <lijia577@ubuntu.(none)>
2014-04-05 16:54:26 -0400

    Created makeMove

---
+++ makeMove.c
@@ -0,0 +1,27 @@
+#include "makeMoves.h"
+#include "main.h"
+
+
+void update(){
+}
+
+void crawler(){
+
+}
+
+bool moveExist(){
+
+}
+
+void scoreKeep(){
+
+}
+
+void countingFlips(){
+
+}
+
+void computerMove(){
+
+
+}
