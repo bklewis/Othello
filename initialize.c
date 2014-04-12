ef7d7a012373ac5088c5626ed690c9dc80de2f35
Williams <ewilliam03@powerpuff.cs.brynmawr.edu>
2014-04-12 09:00:11 -0400

    Put the code I wrote for intitalize into initialize.c.

--- initialize.c
+++ initialize.c
@@ -1,9 +1,16 @@
-#include "initialize.h"
+include "main.h"
+include "initialize.h"
 
-void initialize(){
-	for (int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
-			board[i][j] = ' ';
-		}
-	}
+void initialize() {
+        int i, j;
+        for (i = 0; i < 8; i++) {
+                for (j = 0; j < 8; j++) {
+                        board[i][j] = '-';
+                }
+        }
+        board[3][3] = 'X';
+        board[3][4] = 'O';
+        board[4][3] = 'O';
+        board[4][4] = 'X';
 }
+
