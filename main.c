71b8f3a9b7c4db6c25ded2f3d99c0207467c86f4
Bryce Lewis <bryce.k.lewis@gmail.com>
2014-04-15 14:30:12 -0400

    Starting to configure main to mesh with other functions.

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include "initialize.h"
-#include "io.h"
-#include "makeMove.h"
+//#include "initialize.h"
+//#include "io.h"
+//#include "makeMove.h"
 
 
-char board[8][8];
+//char board[8][8];
 
 int main(){
+
+	srand(time(NULL));
 
 	printf("Othello!\n");
 	return 0;
