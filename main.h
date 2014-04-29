e837db6182458ebfbeb8e011f16644771327bed0
Bryce Lewis <bryce.k.lewis@gmail.com>
2014-04-28 22:25:37 -0400

    Adding commentary

--- main.h
+++ main.h
@@ -10,10 +10,9 @@
 
 #define N 8
 
-int computeCount[N][N];
 char board[N][N];
+int computeCount[N][N];
 int legalMoves[N*N];
-
 
 int p1score; //player socre
 int p2score;
@@ -25,16 +24,13 @@ int bonusmode;
 int bonus_x;
 int bonus_y;
 
-
 typedef enum {X = 1, O = 2, M = 3} token;
 typedef enum {COUNTONLY,FLIPONLY} mode;
 typedef enum {HUMAN=1,COMPUTER=2} playmode;
 typedef enum {BONUSON=1,BONUSOFF=0} bonus;
 
-
 void human(int turn);
 void computer(int turn);
 void chooseMode(int mode1, int mode2);
-
 
 #endif
