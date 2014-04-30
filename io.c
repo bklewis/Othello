6a3130abf3778efaf30ff90a58d7b8610d0daf6b
Bryce Lewis <bryce.k.lewis@gmail.com>
2014-04-30 11:58:18 -0400

    Fixed error in io.c

--- io.c
+++ io.c
@@ -2,14 +2,14 @@
 #include "main.h"
 #include "makeMove.h"
 
-//COLORS! to use then, do   printf( RED "whatever you want print" RESET );
-//#define RED     "\x1b[31m"
-//#define GREEN   "\x1b[32m"
-//#define YELLOW  "\x1b[33m"
-//#define BLUE    "\x1b[34m"
-//#define MAGENTA "\x1b[35m"
-//#define CYAN    "\x1b[36m"
-//#define RESET   "\x1b[0m"
+//COLORS to use then, do   printf( RED "whatever you want print" RESET );
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
 
 //prints the board and simultaneously keeps score
 void print() {
@@ -36,7 +36,7 @@ void print() {
                             else printf("%c ", getTokenName(board[i][j])); 
                 		   }
                 		   
-                }
+                //}
                 printf("| \n");
         }
         printf("   -----------------\n\n");
